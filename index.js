c6165ee40f1a153d33a7317286a812f01c72e9a4
janeishahu <janeishahu@aol.com>
2021-08-10 19:39:29 -0400

    part two

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (request, response) => {
 app.get('/teams/:id', (request, response) => {
   // return the team with the id that matches the param from the request. That is done with the request.params
 
-  // make a request to team 19 by going through each object in the teams array and return the team that matches the id from the request
+  // make a request to team 19 by going through each object in the teams array and return the team that matches the id from the request. For example, when localhost:1337/teams/19 is typed into the browswer(front-end), it is stored in request.params.id. As the teams array is being filter, when it comes upon 19, 19 will be stored in the teamObject variable and returned. What's returned is the Philadelphia Eagles because they are team 19. It will show in the browser. 
 
   const teamObj = teams.filter(team => team.id == request.params.id)
 
